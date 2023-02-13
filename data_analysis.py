bb3d7b816587f2d62766f30ad08546a2de4e96fb
MHSoffar <dxmhss@gmail.com>
2023-02-13 10:43:56 +0300

    my first upload

---
+++ data_analysis.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+st.set_page_config(page_title="Progress Report" , page_icon=':bar_chart:')
+
+df1=pd.read_excel(io=r"data.xlsx",
+                engine='openpyxl',
+                sheet_name='data',
+                usecols='A:H',
+                nrows=100000)
+
+
+
+st.header("Tower Sorting Out :blue [Monthly Report]")
+
+
+
+st.sidebar.header("Please Filtter Down Here :bych: :star:")
+
+team = st.sidebar.multiselect("Team",df1["Team"].unique())
+# usedas = st.sidebar.multiselect("Being_Used_As",df1["Being_Used_As"].unique())
+# reporting_date = st.sidebar.multiselect("Repoting_Date",df1["Repoting_Date"].unique())
+
+
+
+# df_selected=df1.query("Team == @team & Being_Used_As == @usedas & Repoting_Date == @reporting_date" ) 
+
+df_selected=df1.query("Team == @team") 
+st.dataframe(df_selected)
+
+
+
+
+
+
+
+
+
+
+
+
